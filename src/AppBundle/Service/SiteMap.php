b8f94cdd5a8c8eba99a0d669dfd1d560bad7681b
Freddy RAMOS <freddy.ramos94@gmail.com>
2017-09-25 08:40:52 +0200

    remove news block

--- src/AppBundle/Service/SiteMap.php
+++ src/AppBundle/Service/SiteMap.php
@@ -33,15 +33,15 @@ class SiteMap
             'priority' => 1,
         ];
 
-        $news = $this->em->getRepository('AppBundle:News')->getNews()->getResult();
-        foreach ($news as $new) {
-            $date = ($new->getPublishAt()) ? $new->getPublishAt() : $new->getCreatedAt();
-            $urls[] = [
-                'loc' => $this->router->generate('new', ['slug' => $new->getSlug()], Router::ABSOLUTE_URL),
-                'lastmod' => $date->format('Y-m-d'),
-                'priority' => 0.8,
-            ];
-        }
+//        $news = $this->em->getRepository('AppBundle:News')->getNews()->getResult();
+//        foreach ($news as $new) {
+//            $date = ($new->getPublishAt()) ? $new->getPublishAt() : $new->getCreatedAt();
+//            $urls[] = [
+//                'loc' => $this->router->generate('new', ['slug' => $new->getSlug()], Router::ABSOLUTE_URL),
+//                'lastmod' => $date->format('Y-m-d'),
+//                'priority' => 0.8,
+//            ];
+//        }
 
         $urls[] = [
             'loc' => $this->router->generate('accompagnement-international', [], Router::ABSOLUTE_URL),
@@ -53,10 +53,10 @@ class SiteMap
             'priority' => 0.7,
         ];
 
-        $urls[] = [
-            'loc' => $this->router->generate('actualite', [], Router::ABSOLUTE_URL),
-            'priority' => 0.6,
-        ];
+//        $urls[] = [
+//            'loc' => $this->router->generate('actualite', [], Router::ABSOLUTE_URL),
+//            'priority' => 0.6,
+//        ];
 
         $urls[] = [
             'loc' => $this->router->generate('mention-legale', [], Router::ABSOLUTE_URL),
